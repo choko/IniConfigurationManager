e86f8b06973e401f9d0b939bdb9030f3703388f9
Juraj Hamornik <jurko@bdi.sk>
2011-04-29 17:23:31 +0200

    ConfigValidator pokus submitu

---
+++ src/iniconfigurationmanager/ConfigValidator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package iniconfigurationmanager;
+
+/**
+ *
+ * @author KlonK
+ */
+public class ConfigValidator {
+
+    public  boolean  validateStrict() {
+    return false;
+    }
+
+    public boolean validateRelax() {
+    return false;
+    }
+}
