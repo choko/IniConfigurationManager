cab5187ae1dde66fce28f5a4c550b5c8ff620c69
Ondřej Klejch <ondrej.klejch@gmail.com>
2011-05-04 16:23:43 +0200

    Basic ValueLink support added

--- src/iniconfigurationmanager/LinkVisitor.java
+++ src/iniconfigurationmanager/LinkVisitor.java
@@ -6,24 +6,49 @@
 package iniconfigurationmanager;
 
 import iniconfigurationmanager.items.ConfigItem;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
  * @author Ondrej Klejch <ondrej.klejch@gmail.com>
  */
-public class LinkVisitor {
+public class LinkVisitor<T> {
+
+    private T type;
+
+    private HashSet< ConfigItem > enteredConfigItems;
+
+    private List< T > values;
+
+    public LinkVisitor( T type ) {
+        this.type = type;
+        this.values = new LinkedList< T >();
+    }
 
 
     public void enter( ConfigItem item ) {
-        
+        if( enteredConfigItems.contains( item ) ) {
+            //@TODO throw new exception
+        }
+
+        enteredConfigItems.add( item );
     }
 
-    public void visit( Object o ) {
-        
+    
+    public void visit( Object value ) {
+        values.add( (T) value);
     }
 
+    
     public void leave( ConfigItem item ) {
-        
+        enteredConfigItems.remove( item );
+    }
+
+
+    public List<T> getValues() {
+        return values;
     }
 
 }
--- src/iniconfigurationmanager/ValueLink.java
+++ src/iniconfigurationmanager/ValueLink.java
@@ -5,6 +5,7 @@
 
 package iniconfigurationmanager;
 
+import iniconfigurationmanager.items.ConfigItem;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,12 +15,53 @@ import java.util.List;
  */
 public class ValueLink {
 
+    private String sectionName;
 
+    private String itemName;
+
+    private ConfigData configuration;
+
+    
     public ValueLink( String link, ConfigData configuration ) {
-        
+        this.sectionName = getSectionName( link );
+        this.itemName = getItemName( link );
+        this.configuration = configuration;
     }
 
+
+    private String getSectionName( String link ) {
+        return link.substring( 2, link.indexOf("#") );
+    }
+
+
+    private String getItemName( String link ) {
+        return link.substring( link.indexOf("#"), link.length() - 1 );
+    }
+    
+
     public <T> List< T > getValues( T type ) {
-        return new LinkedList< T >();
+        LinkVisitor< T > visitor = new LinkVisitor< T >( type );
+        getLinkedItem().accept( visitor );
+
+        return visitor.getValues();
+    }
+
+
+    public ConfigSection getLinkedSection() {
+        if( configuration.hasSection( sectionName ) ) {
+            return configuration.getSection( sectionName );
+        } else {
+            return null;
+        }
+    }
+
+    public ConfigItem getLinkedItem() {
+        ConfigSection section = getLinkedSection();
+
+        if( section.hasItem( itemName ) ) {
+            return section.getItem( itemName );
+        } else {
+            return null;
+        }
     }
 }
