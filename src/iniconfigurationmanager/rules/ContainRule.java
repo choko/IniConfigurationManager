f1d37530b69cfb0317daae3741d3b47e339cfbff
Juraj Hamornik <jurko@bdi.sk>
2011-05-07 10:28:21 +0200

    added rules

--- src/iniconfigurationmanager/rules/ContainRule.java
+++ src/iniconfigurationmanager/rules/ContainRule.java
@@ -7,12 +7,23 @@ package iniconfigurationmanager.rules;
 
 import iniconfigurationmanager.items.ConfigItemFormatDefinition;
 import iniconfigurationmanager.validators.ValidationResult;
+import java.util.List;
 
 /**
  *
  * @author KlonK
  */
 public class ContainRule implements  ValidationRule {
+
+    String subString;
+    Object subObject;
+    public ContainRule( String subString ) {
+        this.subString = subString;
+    }
+
+    public ContainRule( Object subOject ) {
+        this.subObject = subOject;
+    }
 
     public boolean isAplicableOn(ConfigItemFormatDefinition format) {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -22,4 +33,27 @@ public class ContainRule implements  ValidationRule {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    public ValidationResult validationResult( String value ) {
+        ValidationResult result = new ValidationResult();
+        if (value.contains( subString ) ) {
+            result.addResult( true );
+        } else {
+            result.addResult( false );
+            result.addErrorMsg("Substring missing");
+        }
+
+        return result;
+    }
+
+    public<T> ValidationResult validate( List<T> value) {
+        ValidationResult result = new ValidationResult();
+        if (value.contains( subObject ) ) {
+            result.addResult( true );
+        } else {
+            result.addResult( false );
+            result.addErrorMsg("Item missing");
+        }
+
+        return result;
+    }
 }
--- src/iniconfigurationmanager/rules/MaxValueRule.java
+++ src/iniconfigurationmanager/rules/MaxValueRule.java
@@ -5,10 +5,40 @@
 
 package iniconfigurationmanager.rules;
 
+import iniconfigurationmanager.items.ConfigItemFormatDefinition;
+import iniconfigurationmanager.validators.ValidationResult;
+
 /**
  *
  * @author KlonK
  */
-public class MaxValueRule {
+public class MaxValueRule implements ValidationRule {
+
+    int maxValue;
+
+    public MaxValueRule( int maxValue ) {
+        this.maxValue = maxValue;
+    }
+
+    public boolean isAplicableOn(ConfigItemFormatDefinition format) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public <T> ValidationResult validate(T value) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ValidationResult validate(int value) {
+        ValidationResult result = new ValidationResult();
+        if ( ( this.maxValue < value ) ) {
+            result.addResult(true);
+        }
+        else {
+           result.addResult(false);        
+          result.addErrorMsg("Value too large");
+           }
+        
+        return result;
+    }
 
 }
