d2169d99a9f9307c35928606a114227694d33115
Juraj Hamornik <jurko@bdi.sk>
2011-05-07 14:03:46 +0200

    fix RangeRule

--- src/iniconfigurationmanager/rules/RangeRule.java
+++ src/iniconfigurationmanager/rules/RangeRule.java
@@ -46,61 +46,60 @@ public class RangeRule implements ValidationRule {
 
     
     public ValidationResult validate(OptionData option) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        ValidationResult result = new ValidationResult();
+        result.addErrorMsg("Rule applicated on unsuporter Data");
+        return result;
     }
 
     public ValidationResult validate (SignedOptionData option ) {
         ValidationResult result = new ValidationResult();
-        Integer optionIntValue = option.getValues(new Integer(0)).get(0);
-        if ( optionIntValue > (Integer) this.max ||
-             optionIntValue < (Integer) this.min   ) {
-            result.addResult( true );
-        } else {
-            result.addResult( false );
-            if ( optionIntValue > (Integer) this.max ) {
-                result.addErrorMsg( "Value too large");
+        List<Integer> optionIntValue = option.getValues(new Integer(0));
+        for (Integer integer : optionIntValue) {
+            if ( integer > (Integer) this.max ||
+                 integer < (Integer) this.min   ) {
+            } else {
+                if ( integer > (Integer) this.max ) {
+                    result.addErrorMsg( "Value too large");
+                }
+                if ( integer < (Integer) this.min ) {
+                    result.addErrorMsg( "Value too low");
+                }
             }
-            if ( optionIntValue < (Integer) this.min ) {
-                result.addErrorMsg( "Value too low");
-            }
-
-
         }
         return result;
-
     }
 
     public ValidationResult validationResult ( FloatOptionData option ) {
         ValidationResult result = new ValidationResult();
-        Float optionFloatValue = option.getValues(new Float(0)).get(0);
-        if ( optionFloatValue > ( Float ) this.max ||
-             optionFloatValue < ( Float ) this.min   ) {
-            result.addResult( true );
-        } else {
-            result.addResult( false );
-            if ( optionFloatValue > ( Float ) this.max ) {
-                result.addErrorMsg( "Value too large");
-            }
-            if ( optionFloatValue < ( Float ) this.min ) {
-                result.addErrorMsg( "Value too low");
+        List<Float> optionFloatValue = option.getValues(new Float(0));
+        for (Float floatValue : optionFloatValue) {
+            if ( floatValue > (Float) this.max ||
+                 floatValue < (Float) this.min   ) {
+            } else {
+                if ( floatValue > (Float) this.max ) {
+                    result.addErrorMsg( "Value too large");
+                }
+                if ( floatValue < (Float) this.min ) {
+                    result.addErrorMsg( "Value too low");
+                }
             }
         }
         return result;
     }
     
     public ValidationResult validationResult ( UnsignedOptionData option ) {
-             ValidationResult result = new ValidationResult();
-        Long optionLongValue = option.getValues(new Long(0)).get(0);
-        if ( optionLongValue > ( Long ) this.max ||
-             optionLongValue < ( Long ) this.min   ) {
-            result.addResult( true );
-        } else {
-            result.addResult( false );
-            if ( optionLongValue > ( Long ) this.max ) {
-                result.addErrorMsg( "Value too large");
-            }
-            if ( optionLongValue < ( Long ) this.min ) {
-                result.addErrorMsg( "Value too low");
+        ValidationResult result = new ValidationResult();
+        List<Long> optionLongValue = option.getValues(new Long(0));
+        for (Long longValue : optionLongValue) {
+            if ( longValue > (Long) this.max ||
+                 longValue < (Long) this.min   ) {
+            } else {
+                if ( longValue > (Long) this.max ) {
+                    result.addErrorMsg( "Value too large");
+                }
+                if ( longValue < (Long) this.min ) {
+                    result.addErrorMsg( "Value too low");
+                }
             }
         }
         return result;
